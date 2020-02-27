4c65f156220cbf6e26572854302402656bc49fea
GMV <gmv@GMVs-MacBook-Pro.local>
2020-02-27 13:46:48 +0700

    set activity second text

--- app/src/main/java/latihan/kotlin/twoactivities/SecondActivity.kt
+++ app/src/main/java/latihan/kotlin/twoactivities/SecondActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
@@ -13,9 +14,12 @@ class SecondActivity : AppCompatActivity() {
         setContentView(R.layout.activity_second)
 
         val bundle : Bundle? = intent.extras
-        bundle?.getString("key")
+        val msg = bundle?.getString("key")
 
-        text_message
+        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show()
+
+        text_message.text = msg
+
 
     }
 }
